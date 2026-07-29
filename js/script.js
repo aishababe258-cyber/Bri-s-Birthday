3349eea5353538bb642c61499f4ea4e05987f840
aishababe258-cyber <aishababe258@gmail.com>
2026-07-28 18:52:22 -0700

    Modified js/script.js file

--- js/script.js
+++ js/script.js
@@ -460,3 +460,77 @@ cursor.style.transform=
 
 
 });
+
+/* ===================================================
+   ACTIVE NAVBAR ON SCROLL
+=================================================== */
+
+
+const sections =
+document.querySelectorAll(
+"section[id]"
+);
+
+
+const navLinks =
+document.querySelectorAll(
+".navbar a"
+);
+
+
+
+window.addEventListener(
+"scroll",
+()=>{
+
+
+let current = "";
+
+
+sections.forEach(section=>{
+
+
+const sectionTop =
+section.offsetTop - 150;
+
+
+const sectionHeight =
+section.offsetHeight;
+
+
+if(
+window.scrollY >= sectionTop &&
+window.scrollY < sectionTop + sectionHeight
+){
+
+current = section.getAttribute("id");
+
+}
+
+
+});
+
+
+
+navLinks.forEach(link=>{
+
+
+link.classList.remove("active");
+
+
+
+if(
+link.getAttribute("href")
+===
+"#"+current
+){
+
+link.classList.add("active");
+
+}
+
+
+});
+
+
+});
